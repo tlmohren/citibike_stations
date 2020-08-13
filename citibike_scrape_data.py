ca53128c83608c2e197354c2196de7934293349a
Thomas Mohren <tlmohren@gmail.com>
2020-08-13 06:23:44 -0400

    save periodically

--- citibike_scrape_data.py
+++ citibike_scrape_data.py
@@ -4,32 +4,43 @@ import time
 import sys
 import datetime
 
+# save constants
 SOURCE_URL = "http://gbfs.citibikenyc.com/gbfs/gbfs.json"
 SLEEP_DURATION = 10
+COLLECTION_SECONDS = 60
+SAVE_SPAN_SECONDS = 20
 
+# pre-process data source
 data_urls = requests.get(SOURCE_URL).json()
 station_status_url = data_urls["data"]["en"]["feeds"][2]["url"]
 
-# initialize data dictionary
-data_dict = {}
-station_status = requests.get(station_status_url).json()
-for station in station_status["data"]["stations"]:
-    station_id = station["station_id"]
-    station_time = station["last_reported"]
 
-    subsubdict = {
-        "num_bikes_available": station["num_bikes_available"],
-        "num_ebikes_available": station["num_ebikes_available"],
-    }
-    subdict = {station_time: subsubdict}
-    data_dict[station_id] = subdict
+# define functions ------------
+def initialize_data_dict(station_status_url):
+    data_dict = {}
+    station_status = requests.get(station_status_url).json()
+    for station in station_status["data"]["stations"]:
+        station_id = station["station_id"]
+        station_time = station["last_reported"]
 
+        subsubdict = {
+            "num_bikes_available": station["num_bikes_available"],
+            "num_ebikes_available": station["num_ebikes_available"],
+        }
+        subdict = {station_time: subsubdict}
+        data_dict[station_id] = subdict
+
+    return data_dict
+
+
+# start loop -----------------------------------
 starttime = datetime.datetime.now()
+
+data_dict = initialize_data_dict(station_status_url)
+
 while True:
     station_status = requests.get(station_status_url).json()
-
     for station in station_status["data"]["stations"]:
-
         station_id = station["station_id"]
         station_time = station["last_reported"]
         if station_time not in data_dict[station_id]:
@@ -40,15 +51,23 @@ while True:
             subdict = {station_time: subsubdict}
             olddict = data_dict[station_id]
             data_dict[station_id] = {**olddict, **subdict}
+
+    # sleep until next request
     time.sleep(SLEEP_DURATION)
-
     run_duration = datetime.datetime.now() - starttime
-    if run_duration.seconds / 60 >= 1.0:
-        break
-
     print(f"Run duration: {run_duration.seconds}")
 
+    # Save data and reset data dict periodically
+    if (run_duration.seconds % SAVE_SPAN_SECONDS) == 0:
+        now_time = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
+        filename = "data/citibike_stations_" + now_time + ".json"
+        print(f"Running for {run_duration.seconds}, Saving file: {filename}")
+        with open(filename, "w") as outfile:
+            json.dump(data_dict, outfile)
+        data_dict = initialize_data_dict(station_status_url)
 
-with open("station_data.json", "w") as outfile:
-    json.dump(data_dict, outfile)
+    # break loop after end of duration
+    if run_duration.seconds >= COLLECTION_SECONDS:
+        print("End of script")
+        break
 
